71d877343745cb02a05bf54becc79c1993d40fcc
Taj Ahuja <tajahuja.itprofessional@gmail.com>
2026-08-29 21:03:23 +0530

    Add initial C++ DSA setup

---
+++ 01_Basics/Product.cpp
@@ -0,0 +1,15 @@
+//Program to print product of 3 Numbers by user input   
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter the first number: "<<endl;
+    cin>>a;
+    cout<<"Enter the second number: "<<endl;
+    cin>>b;
+    cout<<"Enter the third number: "<<endl;
+    cin>>c;
+    cout<<"The Product of numbers is : "<<a*b*c <<endl;
+
+    return 0;
+}
